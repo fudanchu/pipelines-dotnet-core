11eba5c100a80d46bb04ff704df312a1704b6121
Dan <bronsondl@state.gov>
2021-08-13 19:48:28 -0400

    Tweaking for .NET 6 usage!

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,9 @@ namespace pipelines_dotnet_core
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options => {
+                options.EnableEndpointRouting = false;
+            }); //.SetCompatibilityVersion(CompatibilityVersion.Latest);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
